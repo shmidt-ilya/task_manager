be9ccd6876deecb5941730b5d32dc5bbbbbbdab9
kurtasov <kurtasov@users.noreply.github.com>
2024-09-21 19:58:40 +0300

    Implemented minimal API to save task data to a CSV file
    1. In app/routes/task.py, a handler for a POST request to add a new task is created using APIRouter (see the prefix in params). For the route to work, main.py is using include_router().
    2. For the route handler to return 201 on successful task creation, fastapi.status is used (see the status_code in the .post decorator params).
    3. For data validation, we employ pydantic. A schema is imported to app/route/task.py to validate incoming task data. The app/schemas/task.py file declares the required data types for the 'TaskCreate' object. Note that this type of object is received by create_task() -- this can be shown by debugging this function and calling type(task).
    4. The create_task() function calls write_task_to_csv(), which simply writes data to CSV and adds an ID to the task row.
    5. To test the API, use the SwaggerUI and cURL.
    6. Note the route handler returns 'task', which is rendered as JSON. See https://fastapi.tiangolo.com/advanced/response-directly/ if you need to return a custom response, such as {"status": "success"}.
    7. Later on we can implement endpoints to list all tasks, all tasks with a given assignee, the task with a specific ID (to be added to the schema as an auto-incremented field).

--- app/data_handler.py
+++ app/data_handler.py
@@ -1,8 +1,15 @@
 import csv
 
 def write_task_to_csv(task):
-    with open('tasks.csv', 'a', newline='') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=task.dict().keys())
+    with open('tasks.csv', 'a+', newline='') as csvfile:
+        fields = list(task.dict().keys())
+        fields.append('task_id')
+        writer = csv.DictWriter(csvfile, fieldnames=fields)
+        data = task.dict()
         if csvfile.tell() == 0:
             writer.writeheader()
-        writer.writerows([task.dict()])
+            data['task_id'] = 1
+        else:
+            csvfile.seek(1)
+            data['task_id'] = sum(1 for line in csv.reader(csvfile))
+        writer.writerows([data])
