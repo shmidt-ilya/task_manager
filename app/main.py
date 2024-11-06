424b437a1b2a44dede72235f1a8a927ae5cb2ade
kurtasov <kurtasov@users.noreply.github.com>
2024-11-06 23:08:39 +0300

    Documentation: added OpenAPI info

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
 from app.routes import task
 
-app = FastAPI()
+app = FastAPI(
+    title="Система управления задачами",
+    description="Простейшая система управления задачами, основанная на "
+                "фреймворке FastAPI.",
+    version="0.0.1",
+    contact={
+        "name": "Цифровая кафедра МФТИ",
+        "url": "https://mipt.ru",
+        "email": "digitaldepartments@mipt.ru",
+    },
+    license_info={
+        "name": "MIT",
+        "url": "https://opensource.org/licenses/MIT",
+    }
+)
 
 
 app.include_router(task.router)
