f18e708c0f3e2b57843d860d47d6fca019c2f5d7
kurtasov <kurtasov@users.noreply.github.com>
2024-11-10 18:00:45 +0300

    GET method for DB + stubs for GET by ID, PATCH and DELETE

--- app/routes/task_v2.py
+++ app/routes/task_v2.py
@@ -1,9 +1,9 @@
-from fastapi import APIRouter, status, Depends
+from fastapi import APIRouter, status, Depends, HTTPException
 from sqlalchemy import text
 from sqlmodel import Session, select, SQLModel
 from app.db import get_session, engine
 from ..schemas import task as schema_task
-from typing import Annotated
+from typing import Annotated, List
 from ..api_docs import request_examples
 
 router = APIRouter(prefix="/v2/tasks", tags=["Tasks"])
@@ -42,3 +42,42 @@ def create_task(task: Annotated[
     session.commit()
     session.refresh(new_task)
     return new_task
+
+
+@router.get("/", status_code=status.HTTP_200_OK,
+            response_model=List[schema_task.TaskRead])
+def read_tasks(session: Session = Depends(get_session)):
+    tasks = session.exec(select(schema_task.Task)).all()
+    if tasks is None or len(tasks) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_204_NO_CONTENT,
+            detail=f"The task list is empty."
+        )
+    return tasks
+
+
+@router.get("/{task_id}", response_model=schema_task.TaskRead)
+def read_task_by_id(task_id: int):
+    raise HTTPException(
+        status_code=status.HTTP_501_NOT_IMPLEMENTED,
+        detail=f"Unable to read task with ID {task_id}: "
+               f"method is not implemented yet."
+    )
+
+
+@router.patch("/{task_id}", status_code=status.HTTP_200_OK, response_model=schema_task.TaskRead)
+def update_task_by_id(task_id: int, data_for_update: dict):
+    raise HTTPException(
+        status_code=status.HTTP_501_NOT_IMPLEMENTED,
+        detail=f"Unable to update task with ID {task_id}: "
+               f"method is not implemented yet."
+    )
+
+
+@router.delete("/{task_id}", status_code=status.HTTP_200_OK)
+def delete_task_by_id(task_id: int):
+    raise HTTPException(
+        status_code=status.HTTP_501_NOT_IMPLEMENTED,
+        detail=f"Unable to delete task with ID {task_id}: "
+               f"method is not implemented yet."
+    )
