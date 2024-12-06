69a325aba53b88fa59b9f22847b39fbd6fb6d834
kurtasov <kurtasov@users.noreply.github.com>
2024-12-07 01:11:58 +0300

    Added signup route

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import (task, task_v2, utils, async_routes)
+from app.routes import (task, task_v2, utils, async_routes, auth)
 #from contextlib import asynccontextmanager  # Uncomment if you need to create tables on app start >>>
 #from app.db import init_database
 #
@@ -30,3 +30,4 @@ app.include_router(task.router)
 app.include_router(task_v2.router)
 app.include_router(utils.router)
 app.include_router(async_routes.router)
+app.include_router(auth.router)
