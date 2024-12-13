70b93513d021e30d0187c69e066bbb5ae59e6462
kurtasov <kurtasov@users.noreply.github.com>
2024-12-13 17:16:34 +0300

    Added tests

---
+++ tests/test_auth.py
@@ -0,0 +1,37 @@
+from fastapi.testclient import TestClient
+import faker
+from app.main import app
+
+client = TestClient(app)
+fake = faker.Faker()
+
+client.fake_user_email = fake.email()
+client.fake_user_password = fake.password()
+client.fake_user_name = fake.first_name()
+client.new_user_id = 0
+client.auth_token = ""
+
+
+def test_signup():
+    response = client.post("/auth/signup",
+                           json={"email": client.fake_user_email,
+                                 "password": client.fake_user_password,
+                                 "name": client.fake_user_name}
+    )
+    assert response.status_code == 201
+    client.new_user_id = response.json()
+
+
+def test_login():
+    response = client.post("/auth/login",
+                           data={"username": client.fake_user_email,
+                                 "password": client.fake_user_password}
+    )
+    assert response.status_code == 200
+    client.auth_token = response.json()['access_token']
+
+
+def test_me():
+    response = client.get("/utils/me", headers={"Authorization": f"Bearer {client.auth_token}"})
+    assert response.status_code == 200
+    assert response.json() == client.new_user_id
